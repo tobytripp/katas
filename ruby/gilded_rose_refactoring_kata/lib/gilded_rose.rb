f5b19a87a43d351a819b531d30f2717ddaaa0828
Toby Tripp <toby.tripp+github@gmail.com>
2023-03-02 17:05:19 -0600

    Hi-lite remaining duplication

--- ruby/gilded_rose_refactoring_kata/lib/gilded_rose.rb
+++ ruby/gilded_rose_refactoring_kata/lib/gilded_rose.rb
@@ -12,18 +12,18 @@ end
 class Normal < Inventory
   def tick()
     item.quality -= 1 if item.sell_in <= 0
-    item.sell_in -= 1
     item.quality -= 1
     item.quality = 0 if item.quality < 0
+    item.sell_in -= 1
   end
 end
 
 class Brie < Inventory
   def tick()
-    item.sell_in -= 1
     item.quality += 1
     item.quality += 1 if item.sell_in <= 0
     item.quality = [item.quality, 50].min
+    item.sell_in -= 1
   end
 end
 

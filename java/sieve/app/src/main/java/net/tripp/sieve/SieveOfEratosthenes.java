5c84bd97a05951c7e72d1adf27f359fa8f6bd9c9
Toby Tripp <toby.tripp+github@gmail.com>
2023-12-03 12:46:39 -0600

    Java sieve of Eratosthenes

--- java/sieve/app/src/main/java/net/tripp/sieve/SieveOfEratosthenes.java
+++ java/sieve/app/src/main/java/net/tripp/sieve/SieveOfEratosthenes.java
@@ -1,12 +1,30 @@
 package net.tripp.sieve;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SieveOfEratosthenes {
 
 	public List<Integer> sieve(int max) {
-		return Arrays.asList(2);
+		boolean[] isPrime = new boolean[max + 1];
+		List<Integer> primes = new ArrayList<Integer>();
+		Arrays.fill(isPrime, true);
+		
+		for (int i = 2; i * i <= max; i++) {
+			if (isPrime[i] ) {
+				for (int j = i*i; j <= max; j += i) {
+					isPrime[j] = false;
+				}
+			}
+		}
+		
+		for (int i = 2; i <= max; i++) {
+			if (isPrime[i]) {
+				primes.add(i);
+			}
+		}
+		
+		return primes;
 	}
-
 }
--- java/sieve/app/src/test/java/net/tripp/sieve/SieveOfEratosthenesTest.java
+++ java/sieve/app/src/test/java/net/tripp/sieve/SieveOfEratosthenesTest.java
@@ -15,4 +15,27 @@ public class SieveOfEratosthenesTest {
 		assertEquals(2, primes.get(0).intValue());
 	}
 
+	@Test
+	public void nextPrime() {
+		List<Integer> primes = sieve.sieve(3);
+		assertEquals(3, primes.get(1).intValue());
+	}
+	
+	@Test
+	public void upToTen() {
+		List<Integer> primes = sieve.sieve(10);
+		Integer[] expected = new Integer[] {2, 3, 5, 7};
+		assertArrayEquals(expected, primes.toArray());
+	}
+	
+	@Test
+	public void upToOneHundred() {
+		List<Integer> primes = sieve.sieve(100);
+		Integer[] expected = new Integer[] {
+					2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+					31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+					73, 79, 83, 89, 97
+				};
+		assertArrayEquals(expected, primes.toArray());
+	}
 }
